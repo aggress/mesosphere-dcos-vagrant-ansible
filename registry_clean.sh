ccd9187b593ea20d906236d41ccfbc9d051e1917
Richard Shaw <richard@aggress.net>
2017-09-13 23:07:32 +0100

    Changed Docker regsistry port as it clashes with openvpn testing

--- registry_clean.sh
+++ registry_clean.sh
@@ -5,4 +5,4 @@
 docker kill $(docker ps -aq)
 docker rmi -f $(docker images -aq)
 docker rm /registry
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
+docker run -d -p 10000:5000 --restart=always --name registry registry:2
