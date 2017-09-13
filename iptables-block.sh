741f27a1f7476183f44b1c0f7293b578a1aa5260
Richard Shaw <richard@aggress.net>
2017-09-13 08:22:15 +0100

    Tidy up

--- iptables-block.sh
+++
@@ -1,6 +0,0 @@
-iptables -A OUTPUT -o lo -j ACCEPT
-iptables -A OUTPUT -d 192.168.0.0/16 -j ACCEPT  # replace by your LAN's network(s)
-iptables -A OUTPUT -d 8.8.8.8/32 -j ACCEPT  # replace by your LAN's network(s)
-iptables -A OUTPUT -d 8.8.4.4/32 -j ACCEPT  # replace by your LAN's network(s)
-iptables -A OUTPUT -p tcp \! --syn -j ACCEPT
-iptables -P OUTPUT DROP
